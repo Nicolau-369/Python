72f552c0dd62c02e4b059ec14fce3cbcdb2ca286
Nic <160781135+Nicolau-369@users.noreply.github.com>
2024-02-23 10:58:18 -0300

    Update classe-Counter.py

--- classe-Counter.py
+++ classe-Counter.py
@@ -4,19 +4,19 @@ from collections import Counter
 
 def main():
     # Uma lista de estudantes na turma A
-    turma_a = ["Bárbara", "João", "Carlos", "Dário", "Priscila", "Ana"
-               "Kevin", "João", "Marina", "Bianca", "Gustavo", "Fernanda"]
+    turma_a = ["Malik", "Nicolau", "Magnolia", "Josias", "Morgana", "Andromeda"
+               "Maria", "Rex", "Kletus", "Trask", "Medici", "Sun"]
 
     # Uma lista de estudantes na turma B
-    turma_b = ["Hiro", "Bruno", "Claudia", "Débora", "Fernanda", "Gabriela",
-               "Leticia", "João", "Jairo", "Samuel", "Taciana", "Gabriel"]
+    turma_b = ["Lorenzo", "Joaquim", "Joaquina", "Suelen", "Joãozinho", "Gabriela",
+               "Leticia", "Valdemir", "Walter", "Bianca", "Tati", "Neusa"]
 
     # Crie um Counter para as turmas A e B
     a = Counter(turma_a)
     b = Counter(turma_b)
 
-    # Quantos estudantes na turma A se chamam João?
-    print(a["João"])
+    # Quantos estudantes na turma A se chamam Nicolau?
+    print(a["Nicolau"])
 
     # Quantos estudantes estão na turma A?
     print(sum(a.values()), "estudantes na turma A")
